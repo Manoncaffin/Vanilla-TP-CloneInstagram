fd05f1cacd2e2efba5c03daa8b9391adc7b7fbb3
alexis-moreno-lopez <alexis.morenolopezpro@gmail.com>
2024-01-09 13:52:31 +0100

    alexis avant dernier git

--- js/modal.js
+++ js/modal.js
@@ -29,15 +29,3 @@ function closeModal() {
   modal.classList.add('d-none');
 }
 
-const coeur = document.querySelector("#coeur");
-coeur.addEventListener('click', () => {
-
-  coeur.classList.add('red');
-});
-// document.addEventListener('DOMContentLoaded', function() {
-//   const coeur = document.getElementById('coeur');
-
-//   coeur.addEventListener('click', function() {
-//     coeur.classList.add('red');
-//   });
-// });
--- pages/profil.php
+++ pages/profil.php
@@ -18,8 +18,8 @@ include_once('../process/result_research.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Instagram</title>
-    <link href="../styles.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="../styles.css" rel="stylesheet">
 </head>
 
 <body>
@@ -189,14 +189,19 @@ include_once('../process/result_research.php');
             </div>
             <div class="carre_comment"></div>
             <input class="commentaire" type="text" placeholder="commenter...">
-            <img id="coeur" class="coeur" height="60px " width="60px" src="../img/coeur.jpg" alt="">
+            <!-- <img id="coeur" class="coeur" height="60px " width="60px" src="../img/coeur.jpg" alt=""> -->
+            <div class="border h-25" id="coeur"></div>
             <div><button type="submit" class="btn btn-primary">Go</button></div>
         </div>
     </div>
 
+    
 
-    <script src="../index.js"></script>
+
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="../js/modal.js"></script>
+    <script src="../js/like.js"></script>
 </body>
 
 </html>
